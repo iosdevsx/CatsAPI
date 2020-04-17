1da246be75d7e28dc8d9d8b330a75d916e775456
Yury Loginov <jseverdream@ya.ru>
2020-04-17 16:57:34 +0300

    Initial Commit

---
+++ Caturday/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Caturday
+//
+//  Created by Юрий Логинов on 17.04.2020.
+//  Copyright © 2020 Юрий Логинов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
